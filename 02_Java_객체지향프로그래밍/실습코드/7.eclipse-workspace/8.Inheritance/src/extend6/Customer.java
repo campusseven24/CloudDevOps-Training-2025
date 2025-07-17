2a77667859968882de0e2560281506e2858d71b3
campusseven24 <campusseven24@gmail.com>
2025-07-17 18:13:07 +0900

    20250717_8week

--- 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/Customer.java
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/Customer.java
@@ -1,5 +1,10 @@
 package extend6;
 
+/*
+ * 속성 : 고객 아이디, 고객 이름, 고객 등급, 포인트, 포인트 적립비율
+ * 기능 : showInfo(), 
+ *       물품 구매시 1% 포인트 적립 
+ */
 public class Customer {
 
 }
--- 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/VIPCustomer.java
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/VIPCustomer.java
@@ -1,5 +1,10 @@
 package extend6;
 
+/*
+ * 제품을 살때 10% 할인을 해 줌
+ * 포인트는 제품 가격의 5% 적립해 줌 
+ * 담당 전문 상담원이 배정됨
+ */
 public class VIPCustomer {
 
 }
