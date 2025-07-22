e31b3ef262c5f3d0e9c07859ba19b76b1620ad0a
campusseven24 <campusseven24@gmail.com>
2025-07-22 11:54:34 +0900

    20250722_9week

--- 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/CustomerMain.java
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/CustomerMain.java
@@ -10,4 +10,68 @@ package extend6;
  */
 public class CustomerMain {
 
+	public static void main(String[] args) {
+		//일반 고객
+		Customer customerLee = new Customer();
+		customerLee.setCustomerName("이순신");
+		customerLee.setBonusPoint(1000);
+		
+		System.out.println("=== 일반 고객 정보 ===");
+		System.out.println(customerLee.showInfo());
+		
+		//10,000원 상품 구매
+		int price = 10000;
+		int payAmount = customerLee.calcPrice(price);
+		System.out.println("상품 가격: " +price+"원");
+		System.out.println("지불 금액: " +payAmount+"원");
+		System.out.println("구매 후 정보: " + customerLee.showInfo());
+		
+		//VIP 고객
+		VIPCustomer customerShin = new VIPCustomer();
+		customerShin.setCustomerName("신사임당");
+		customerShin.setBonusPoint(10000);
+		customerShin.setAgentID(12345);
+		
+		System.out.println();
+		
+		System.out.println("===VIP 고객 정보===");
+		System.out.println(customerShin.showInfo());
+		
+		//10,000원 상품 구매
+		int vipPayAmount = customerShin.calcPrice(price);
+		System.out.println("상품 가격:" +price+ "원");
+		System.out.println("할인 적용 지불 금액: " +vipPayAmount+ "원 (10% 할인)");
+		System.out.println("구매 후 정보 : " + customerShin.showInfo());
+		
+		System.out.println();
+		
+		System.out.println("=== 혜택 비교 ===");
+		System.out.println("일반 고객 지불액:" +payAmount+ "원");
+		System.out.println("VIP 고객 지불액:" +vipPayAmount+ "원");
+		System.out.println("VIP 할인 혜택:" +(payAmount - vipPayAmount)+ "원");
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/VIPCustomer.java
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/8.Inheritance/src/extend6/VIPCustomer.java
@@ -5,6 +5,59 @@ package extend6;
  * 포인트는 제품 가격의 5% 적립해 줌 
  * 담당 전문 상담원이 배정됨
  */
-public class VIPCustomer {
+public class VIPCustomer extends Customer {
 
+	private int agentID;
+	private double salesRatio;
+	
+	public VIPCustomer() {
+		customerGrade = "VIP";
+		bonusRatio = 0.05;		// 5% 보너스 적립
+		salesRatio = 0.1;		// 10% 할인
+	}
+
+	public int getAgentID() {
+		return agentID;
+	}
+
+	public void setAgentID(int agentID) {
+		this.agentID = agentID;
+	}
+
+	public double getSalesRatio() {
+		return salesRatio;
+	}
+
+	public void setSalesRatio(double salesRatio) {
+		this.salesRatio = salesRatio;
+	}
+	
+	@Override		// VIP 고객 : 10% 할인 + 5% 보너스 적립
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * salesRatio);
+	}
+	
+	@Override
+	public String showInfo() {
+		return customerName +"님의 등급은 " +customerGrade+
+				"이며, 보너스 포인트는 " +bonusPoint+ 
+				"이고, 담당 상담원 번호는 " +agentID+ "입니다.";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
