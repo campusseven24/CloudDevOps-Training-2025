22ba987c1d0a294b3765ecfc453c53cceb324898
campusseven24 <campusseven24@gmail.com>
2025-07-23 17:23:48 +0900

    20250723_9week

---
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/12.Generic/src/generics/GenericPrinter.java
@@ -0,0 +1,19 @@
+package generics;
+
+// T : 자료형 매개변수 (type parameter)
+public class GenericPrinter<T> {
+	private T material;		// T 자료형으로 선언한 변수
+
+	public T getMaterial() {	// T 자료형 변수 material을 반환하는 제네릭 메서드 
+		return material;
+	}
+
+	public void setMaterial(T material) {
+		this.material = material;
+	}
+	
+	@Override
+	public String toString() {
+		return material.toString();
+	}
+}
---
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/12.Generic/src/generics/GenericPrinterTest.java
@@ -0,0 +1,40 @@
+package generics;
+
+public class GenericPrinterTest {
+
+	public static void main(String[] args) {
+		//제네릭 자료형(Generic Type), 매개변수화된 자료형(parameterized type)
+		//Powder형으로 GenericPrinter 객체 생성
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+		powderPrinter.setMaterial(new Powder());
+		System.out.println(powderPrinter);
+		
+		//             대입된 자료형
+		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+		plasticPrinter.setMaterial(new Plastic());
+		System.out.println(plasticPrinter);
+		
+		//대입된 자료형 <Powder>를 명시하지 않음
+		GenericPrinter powderPrinter2 = new GenericPrinter();
+		powderPrinter2.setMaterial(new Powder());
+		Powder powder = (Powder) powderPrinter2.getMaterial();  //강제 형변환
+		System.out.println(powderPrinter2);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
