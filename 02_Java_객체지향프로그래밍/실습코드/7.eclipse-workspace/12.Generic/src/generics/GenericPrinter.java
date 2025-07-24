8f38928fb12a1c6d8f36cb15e2acb7d9576ad9f9
campusseven24 <campusseven24@gmail.com>
2025-07-24 16:52:13 +0900

    20250724_9week

--- 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/12.Generic/src/generics/GenericPrinter.java
+++ 02_Java_객체지향프로그래밍/실습코드/7.eclipse-workspace/12.Generic/src/generics/GenericPrinter.java
@@ -1,7 +1,7 @@
 package generics;
 
 // T : 자료형 매개변수 (type parameter)
-public class GenericPrinter<T> {
+public class GenericPrinter<T extends Material> {
 	private T material;		// T 자료형으로 선언한 변수
 
 	public T getMaterial() {	// T 자료형 변수 material을 반환하는 제네릭 메서드 
