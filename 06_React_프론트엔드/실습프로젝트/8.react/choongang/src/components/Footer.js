1186c211a53d29bb65d6308a9e33a8e7b9eacecd
campusseven24 <campusseven24@gmail.com>
2025-08-06 17:58:41 +0900

    20250806_11week

--- 06_React_프론트엔드/실습프로젝트/8.react/choongang/src/components/Footer.js
+++ 06_React_프론트엔드/실습프로젝트/8.react/choongang/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
+    <div className='footerStyle'>
       풋터
     </div>
   )
---
+++ 06_React_프론트엔드/실습프로젝트/8.react/choongang/src/components/Footer2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer2() {
+  return (
+    <div className='footerStyle2'>
+      풋터2
+    </div>
+  )
+}
+
+export default Footer2
