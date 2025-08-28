7e2cdcbc891bdb0a38a267b0ff8024703d66315e
campusseven24 <campusseven24@gmail.com>
2025-08-28 18:56:56 +0900

    20250828_14Week

---
+++ 05_백엔드_개발/02_JSP_Servlet/11.eclipse-ee-workspace/servletmvc/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.servletmvc;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("test=========================");
+		request.getRequestDispatcher("/WEB-INF/views/new-form.jsp").forward(request, response);
+	}
+}
+
+
+
+
+
+
+
